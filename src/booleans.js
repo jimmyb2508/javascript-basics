c2fe4413f8d3811e456939a40e11255155484067
jimmyb2508 <james.barrington1985@gmail.com>
2019-10-13 22:02:46 +0100

    answers as of this weekend

--- src/booleans.js
+++ src/booleans.js
@@ -1,61 +1,73 @@
 const negate = a => {
-  // your code here
+  return !a;
 };
 
 const both = (a, b) => {
-  // your code here
+  return a && b;
 };
 
 const either = (a, b) => {
-  // your code here
+  return a || b;
 };
 
 const none = (a, b) => {
-  // your code here
+  return !a && !b;
 };
 
 const one = (a, b) => {
-  // your code here
+  return a!==b;
 };
 
 const truthiness = a => {
-  // your code here
+  return !!a;
 };
 
 const isEqual = (a, b) => {
-  // your code here
+  return a==b;
 };
 
 const isGreaterThan = (a, b) => {
-  // your code here
+    return a > b;
 };
 
 const isLessThanOrEqualTo = (a, b) => {
-  // your code here
+  return a <= b;
 };
 
 const isOdd = a => {
-  // your code here
+  if (a % 2 === 1)
+  {return true;}
+  else {return false;}
 };
 
 const isEven = a => {
-  // your code here
+  if (a % 2 === 0)
+  {return true;}
+  else {return false;}
 };
 
 const isSquare = a => {
-  // your code here
+  if (Number.isInteger(Math.sqrt(a)))
+  {return true;}
+  else {return false;}
 };
 
 const startsWith = (char, string) => {
-  // your code here
+  if (string.startsWith(char))
+  {return true;}
+  else {return false;}
 };
 
 const containsVowels = string => {
-  // your code here
+  if(string.includes('a' || 'e' || 'i' || 'o' || 'u' || 'A' || 'E' || 'I' || 'O' || 'U'))
+  {return true;}
+  else {return false;}
 };
 
 const isLowerCase = string => {
-  // your code here
+  if (string == string.lowerCase())
+  {return true;}
+  else {return false;}
 };
 
 module.exports = {
